e8832e8fd2296a147b7499c515e472619b82bed9
PenAndPapers <steven.pilagan@gmail.com>
2026-09-12 16:08:27 +0800

    fix: initial RBAC implementation

--- backend/fastapi/api/modules/user/service.py
+++ backend/fastapi/api/modules/user/service.py
@@ -5,5 +5,16 @@ class UserService:
   def __init__(self, repository: UserRepository) -> None:
     self.repository = repository
 
+  def get_user_permissions(self, user_id: int) -> set[str]:
+    user = self.repository.get_user_with_permissions(user_id)
+    if not user:
+      return set()
+
+    permissions = set()
+    for role in user.roles:
+      for perm in role.permissions:
+        permissions.add(perm.name)
+    return permissions
+
   def get_user(self) -> None:
     pass
